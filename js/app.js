5a9b8451cff6bbd3c8ff63b466c6155a6365ba67
AntoineCharette <antoineocharette@gmail.com>
2021-07-14 00:50:28 -0400

    adds switch and final alert

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
+/* eslint-disable indent */
 'use strict';
-alert('Please the following using yes or no.');
-// let answer1 = 'yes';
-// let answer2;
-// let answer3;
-// let answer4;
-// let answer5;
+let userName = prompt('Welcome to my site! Please enter your name.');
+alert('You may answer the following using either yes/no or y/n.');
 const answers = ['y', 'n', 'y', 'n', 'y'];
 const prompts = ['Have I played on a State Championship winning football team?', 'Was I born in the United States of America?', 'Did I start university at 16 years old?', 'Have I previously attempted to obtain a bachelor\'s degree in Computer Science?', 'Have I attended culinary school and graduated with honors?'];
-// let resp1 = prompt();
-// let resp2 = prompt();
-// let resp3 = 
 for (let i = 0; i < answers.length; i++) {
   const answer = answers[i];
   const resp = prompt(prompts[i]).charAt(0).toLowerCase();
-
-  // alert(resp);
+  switch (resp === answer) {
+    case true:
+      // console.log('Awesome! You answered correctly!');
+      alert('Awesome! You answered correctly!');
+      break;
+    default:
+      // console.log('Sorry, you answered incorrectly.');
+      alert('Sorry, you answered incorrectly.');
+      break;
+    // default:
+    //   // console.log('You did not enter a proper response');
+    //   alert('You did not enter a proper response.');
+    //   break;
+  }
 }
-// console.log('');
-// alert();
+alert('Thank you, ' + userName + ', for playing my game!');
