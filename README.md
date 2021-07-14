3ee2b12d43f25136dfcdd8516fae8a43dff7f25a
AntoineCharette <antoineocharette@gmail.com>
2021-07-14 12:12:11 -0400

    changes the variable definition to let and takes it out of the scope of the for loop to create it once only

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About Me
 
-<!-- I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app -->
+This project is a page with resume style information and a small biography. It also includes a minigame with questions about me.
 
 ### Author: Antoine Charette
 
--- js/app.js
+++ js/app.js
@@ -4,9 +4,11 @@ let userName = prompt('Welcome to my site! Please enter your name.');
 alert('You may answer the following using either yes/no or y/n.');
 const answers = ['y', 'n', 'y', 'n', 'y'];
 const prompts = ['Have I played on a State Championship winning football team?', 'Was I born in the United States of America?', 'Did I start university at 16 years old?', 'Have I previously attempted to obtain a bachelor\'s degree in Computer Science?', 'Have I attended culinary school and graduated with honors?'];
+let answer;
+let resp;
 for (let i = 0; i < answers.length; i++) {
-  const answer = answers[i];
-  const resp = prompt(prompts[i]).charAt(0).toLowerCase();
+  answer = answers[i];
+  resp = prompt(prompts[i]).charAt(0).toLowerCase();
   switch (resp === answer) {
     case true:
       // console.log('Awesome! You answered correctly!');
